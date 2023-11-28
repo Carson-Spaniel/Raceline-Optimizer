b903bfec8c46e666471f479b31c67e902364f90a
Carson-Spaniel <carsonspaniel@gmail.com>
2023-11-28 14:43:29 -0600

    adding lots of stuff

--- trackAnalyzer.py
+++ trackAnalyzer.py
@@ -1,4 +1,20 @@
 from PIL import Image
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import time
+from collections import deque
+
+class BackgroundColors:
+    RESET = '\033[0m'
+    BLACK = '\033[40m'
+    RED = '\033[41m'
+    GREEN = '\033[42m'
+    YELLOW = '\033[43m'
+    BLUE = '\033[44m'
+    PURPLE = '\033[45m'
+    CYAN = '\033[46m'
+    WHITE = '\033[47m'
 
 def is_black(pixel, threshold=100):
     # Assuming pixel is in RGB format
@@ -12,177 +28,148 @@ def is_black(pixel, threshold=100):
 
 def process_image(image_path, output_file, initial_target_size=(100, 100), output_image_path='output.jpg'):
     images = []
-    # while initial_target_size[0] != 0 and initial_target_size[1] != 0:
-    # Open the image file
     img = Image.open(image_path)
-
-    # Resize the image to the specified target size
     img = img.resize(initial_target_size, resample=Image.BOX)
-
-    # img.save(output_image_path, format='JPEG')
-
-    # Get the size of the resized image
     width, height = img.size
+    results = []
 
-    # Create a new file for storing the results
+    for y in range(height):
+        row = []
+        nodes = deque()
+        for x in range(width):
+            pixel = img.getpixel((x, y))
+            is_black_pixel = is_black(pixel)
+            row.append('1' if is_black_pixel else '0')
+            if is_black_pixel:
+                negX = width - 1
+                while negX != x:
+                    if is_black(img.getpixel((negX, y))):
+                        nodes.append('1')
+                    elif '1' in nodes:
+                        nodes.append('0')
+                    else:
+                        nodes.append('0')
+                    negX -= 1
+                while nodes:
+                    row.append(nodes.pop())
+                break
+        results.append(''.join(row))
+
     with open(output_file, 'w') as f:
-        # Iterate through each pixel
-        for y in range(height):
-            for x in range(width):
-                nodes = []
-                pixel = img.getpixel((x, y))
+        f.write('\n'.join(results))
 
-                # Check if the pixel is considered black
-                if is_black(pixel):
-                    f.write('1')
-                    negX = width-1
-                    while negX != x:
-                        if is_black(img.getpixel((negX, y))):
-                            nodes.append('1')
-                        elif '1' in nodes:
-                            nodes.append('0')
-                        else:
-                            nodes.append('0')
-                        negX -= 1
-                    i = len(nodes)
-                    while i != 0:
-                        f.write(nodes.pop())
-                        i-=1
-                    break
-
-                else:
-                    f.write('0')
-
-            # Add a newline after each row
-            f.write('\n')
-
-    # Read the grid from the text file
     with open(output_file, 'r') as file:
         lines = file.readlines()
         images.append(lines)
-    
-    # hole = False
-    # totalNodes = 0
-    # for line in lines:
-    #     node = 0
-    #     fill = False
-    #     for i in line:
-    #         if i == '1':
-    #             node += 1
-    #             totalNodes += 1
-    #         if i == '2':
-    #             fill = True
-    #     if node == 1:
-    #         hole = True
-    #         break
-    #     # elif node > 5 and not fill:
-    #     #     hole = True
-    #     #     break
 
-    # if totalNodes == 0:
-    #     hole = True            
-
-    # if hole:
-    #     lines = images.pop()
-    #     break
-    # else:
-    #     initial_target_size = (initial_target_size[0]-1,initial_target_size[1]-1)
-
-    # Convert the lines to a 2D list of integers
-    grid = [[int(char) for char in line.strip()] for line in lines]
+    grid = [list(map(int, line.strip())) for line in lines]
 
     return grid
 
-# Example usage with a specified initial target size
-image_path = 'tracks/cota.jpg'
-output_file = 'output.txt'
-initial_target_size = (150, 150)
-grid = process_image(image_path, output_file, initial_target_size, output_image_path='output.jpg')
+def addNodes(grid):
+    # Print the grid for visualization
+    i = 0
+    rowNum = 0
+    xCoords = []
+    yCoords = []
+    for row in grid:
+        rowNumStr = str(rowNum)
+        if rowNum < 100:
+            rowNumStr = '0'+str(rowNum)
+            if rowNum < 10:
+                rowNumStr = '0'+str(rowNumStr)
+        colNum = 0
+        for node in row:
+            if node == 1:
+                i += 1
+                xCoords.append(colNum)
+                yCoords.append(-rowNum+len(grid))
+            colNum += 1
+        rowNum +=1
 
-print('\n')
+    print(f'\nNumber of nodes: {i}')
+    return xCoords,yCoords
 
-# Print the updated grid
-for row in grid:
-    print(' '.join(map(str, row)))
+def plotNodes(xCoords, yCoords):
+    # Assuming x_nodes and y_nodes are lists containing the x and y coordinates of nodes
+    plt.plot(xCoords, yCoords, '.', label='Track Nodes')
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('Track Map with Nodes')
+    plt.legend()
+    plt.show()
 
-print('\n')
+def printTracks(tracksFolder):
+    # Get the absolute path based on the current working directory
+    absoluteTracksFolder = os.path.abspath(tracksFolder)
 
-class BackgroundColors:
-    RESET = '\033[0m'
-    BLACK = '\033[40m'
-    RED = '\033[41m'
-    GREEN = '\033[42m'
-    YELLOW = '\033[43m'
-    BLUE = '\033[44m'
-    PURPLE = '\033[45m'
-    CYAN = '\033[46m'
-    WHITE = '\033[47m'
+    # Get a list of files in the tracks folder
+    trackFiles = [f for f in os.listdir(absoluteTracksFolder) if os.path.isfile(os.path.join(absoluteTracksFolder, f))]
 
-# Print the grid for visualization
-i = 0
-rowNum = 0
-for row in grid:
-    rowNumStr = str(rowNum)
-    if rowNum < 100:
-        rowNumStr = '0'+str(rowNum)
-        if rowNum < 10:
-            rowNumStr = '0'+str(rowNumStr)
-    print(rowNumStr, end=' ')
+    # Print out the available tracks
+    print("\nAvailable Tracks:")
+    for i, trackFile in enumerate(trackFiles, start=1):
+        print(f"{i}. {trackFile.split('.')[0].capitalize()}")
 
-    rowNum +=1
-    for node in row:
-        if node == 1:
-            print(BackgroundColors.BLUE + '   ' + BackgroundColors.RESET, end=' ')
-            i += 1
-        # elif node == 2:
-        #     print(BackgroundColors.RED + '   ' + BackgroundColors.RESET, end=' ')
-        #     i += 1
+    while True:
+        # Prompt the user to choose a track
+        selected_track_index = int(input("\nEnter the number of the track you want to choose: ")) - 1
+        if selected_track_index == -1:
+            print('Invalid choice.')
         else:
-            print(BackgroundColors.GREEN + '   ' + BackgroundColors.RESET, end=' ')
-    print('\n\n', end='')
-print('    ', end='')
-for item in range(len(grid)):
-    itemStr = str(item)
-    if item < 100:
-        itemStr = '0'+str(item)
-        if item < 10:
-            itemStr = '0'+str(itemStr)
-    print(itemStr, end=' ')
+            try:
+                # Retrieve the chosen track filename
+                chosenTrack = trackFiles[selected_track_index]
+                break
+            except Exception as e:
+                print('Selected option not in list.')
 
-print(f'\nNumber of nodes: {i}')
+    print(f"You selected: {chosenTrack.split('.')[0].capitalize()}")
+    return chosenTrack
 
-coords = str(input('Input the coordinates of the starting line in the format rr/cc,rr/cc. Where row is rr and column is cc.\n'))
-direction = str(input('Input the coordinates of direction to go in.\n'))
-allCoords = coords.split(',')
-for coord in allCoords:
-    row = coord.split('/')[0]
-    col = coord.split('/')[1]
-    grid[int(row)][int(col)] = '3'
+def main():
+    def timeEstimate(x):
+        return round(.85*(np.exp(x * 0.00136)) - .85, 2)
+    # Specify the relative path to your tracks folder
+    tracksFolder = 'tracks'
+    while True:
+        image = printTracks(tracksFolder)
+        imagePath = f'{tracksFolder}/{image}'
+        output_file = 'output.txt'  # Output file for the track in 1s and 0s
+        while True:
+            try:
+                size = int(input("\nEnter the size you want: "))
+                if size > 0:
+                    if size < 3000:
+                        break
+                    else:
+                        print("Size is too big. Keep it under 3000.")
+                else:
+                    print("Enter a positive number.")
+            except Exception as e:
+                print("Invalid size.")
+        print('\nBuilding Track...')
+        print(f'Estimated Time is: {timeEstimate(size)} seconds.')
 
-directionRow = direction.split('/')[0]
-directionCol = direction.split('/')[1]
-grid[int(directionRow)][int(directionCol)] = '3'
+        startTime = time.time()
+        grid = process_image(imagePath, output_file, (size, size), output_image_path='output.jpg')
+        endTime = time.time()
+        print(f'Actual time was: {round(endTime-startTime,2)} seconds.')
 
-print('\n')
+        x, y =  addNodes(grid)
+        plotNodes(x, y)
+        more = False
+        while True:
+            choice = str(input('Test another track? (y/n): '))
+            if choice == 'y':
+                more = True
+                break
+            elif choice == 'n':
+                more = False
+                break
+            else:
+                print('Invalid choice.')
+        if not more:
+            break
 
-# Print the updated grid
-for row in grid:
-    print(' '.join(map(str, row)))
-
-print('\n')
-
-# Print the grid for visualization
-i = 0
-for row in grid:
-    for node in row:
-        if int(node) == 1:
-            print(BackgroundColors.BLUE + '  ' + BackgroundColors.RESET, end='')
-            i += 1
-        elif int(node) == 2:
-            print(BackgroundColors.RED + '  ' + BackgroundColors.RESET, end='')
-            i += 1
-        elif int(node) == 3:
-            print(BackgroundColors.PURPLE + '  ' + BackgroundColors.RESET, end='')
-        else:
-            print(BackgroundColors.GREEN + '  ' + BackgroundColors.RESET, end='')
-    print('\n', end='')
+main()
