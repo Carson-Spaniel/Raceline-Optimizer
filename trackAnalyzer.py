d1a3eb9f6937ad1f1bd2784aeb46196471a688d5
Carson-Spaniel <carsonspaniel@gmail.com>
2024-02-26 17:40:02 -0600

    more testing

--- trackAnalyzer.py
+++ trackAnalyzer.py
@@ -305,7 +305,7 @@ def start(x,y,direction, xCoords, yCoords):
     if jNeg:
         j *= -1
 
-    concurrentProcesses = False
+    concurrentProcesses = True
 
     if concurrentProcesses:
         with concurrent.futures.ProcessPoolExecutor() as executor:
